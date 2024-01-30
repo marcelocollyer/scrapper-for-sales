ae457bdbc583f80428bb9234f638bf050492043b
Marcelo Collyer <marcelocollyer@gmail.com>
2024-01-30 08:11:30 -0300

    initial stage - magalu store already working

---
+++ main.py
@@ -0,0 +1,149 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from html2image import Html2Image
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    print("webscrapper is running...")
+    options = Options()
+    options.add_argument("-headless")
+    driver = webdriver.Firefox(options=options)
+
+    url = update.message.text.split()[1]
+    driver.get(url)
+
+    element = driver.find_element(By.CSS_SELECTOR, '[data-testid="heading-product-title"]')
+    productTitle = element.get_attribute('innerHTML')
+    
+    img = driver.find_element(By.CSS_SELECTOR, '[data-testid="image-selected-thumbnail"]')
+    image_src = img.get_attribute('src')
+
+    
+    driver.find_element(By.CLASS_NAME, 'text-button-cookie').click()
+    driver.find_element(By.CSS_SELECTOR, '[data-testid="button-message-box"]').click()
+
+
+    element = driver.find_element(By.CSS_SELECTOR, '[data-testid="mod-productprice"]')
+    element.screenshot('price.png')
+    price_src = '/Users/marcelocollyer/Documents/workspace/Personal/sales-scrapper-py/price.png'
+
+    driver.quit()
+
+    hti = Html2Image()
+
+
+    html = """
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Product Information</title>
+        </head>
+        <style>          
+            .body {
+                margin: 0px;
+            }
+                    
+            .internal-div {
+                margin: 0px 0px 0px 0px;
+                height: 1599px;
+                width: 899px;
+                background-image: url("/Users/marcelocollyer/Documents/workspace/Personal/sales-scrapper-py/default_background.jpg");
+            }
+
+            .product-div {
+                padding: 150px 5px 5px 5px;
+                min-height: 760px;
+            }
+
+            .price-div {
+                
+                box-sizing: border-box;
+                color: rgb(15, 17, 17);
+                display: inline;
+                font-family: Arial, sans-serif;
+                font-size: 28px;
+                height: auto;
+                line-height: normal;
+                text-size-adjust: 100%;
+                width: auto;
+            }
+
+            .old-price-div {
+                text-align: center;
+                font-size: 25px;
+                margin: 0px 0px 0px 0px;
+                color: rgb(86, 89, 89);
+            }
+
+            .price {
+                text-align: center;
+                font-size: 70px;
+                padding-left: 50px;
+            }
+
+            .old-price {
+                text-decoration: line-through;
+            }
+            
+            .title {
+                margin-left: auto;
+                margin-right: auto;
+                text-align: center;
+                max-width: 650px;
+                font-family: serif;
+                font-size: 45px;
+                text-align: left;
+                color: #4a4a4a;
+            }
+
+            .product-img {
+                /* Background pattern from Toptal Subtle Patterns */
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>""" + f"""
+        <body class="body">
+            <div class="internal-div">
+            <div class="product-div">
+                <img src="{image_src}" alt="Product Image" class=product-img height="500">
+                <h1 class="title">{productTitle}</h1>
+            </div>
+            <div class="price-div">
+                <p class="price"><img src="{price_src}" class=product-img width="750px"></p>  
+            </div>
+            </div>
+
+        </body>
+    </html>"""
+
+    # screenshot an HTML string (css is optional)
+    hti.screenshot(html_str=html, save_as='page.png', size=(899, 1599))
+    
+    await context.bot.send_photo(chat_id=update.effective_chat.id,filename='page.png',photo=open("page.png", "rb"))
+
+def main():
+
+    application = ApplicationBuilder().token('6481858651:AAGY3oOoJ7XO3iMAPIrEPe5MTQlYw8VYWq4').build()
+    
+    start_handler = CommandHandler('get', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
+
+
+
+main()
