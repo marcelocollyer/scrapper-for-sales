bda9da5828a07dbd22bebe9dcd361a87c4a77104
Marcelo Collyer <marcelocollyer@gmail.com>
2024-04-12 18:14:39 -0400

    rollback mercado livre to original version

--- mercado_livre.py
+++ mercado_livre.py
@@ -8,28 +8,23 @@ from telegram import Update
 from telegram.ext import ContextTypes
 import os
 from datetime import datetime
-
 def deleteTempFiles(date_time):
     if os.path.exists(f"{date_time}.png"):
         os.remove(f"{date_time}.png")
     else:
         print("The file does not exist")
-
     if os.path.exists(f"price-{date_time}.png"):
         os.remove(f"price-{date_time}.png")
     else:
         print("The file does not exist")
-
     if os.path.exists(f"image-{date_time}.png"):
         os.remove(f"image-{date_time}.png")
     else:
         print("The file does not exist")
-
 async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Processando...")
     print("webscrapper is running...")
     today = datetime.now()
-
     try:
         options = Options()
         options.add_argument('--headless')
@@ -37,39 +32,29 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--window-size=1920,1400")
         driver = webdriver.Chrome(options=options)
-
         url = update.message.text.split()[1]
         driver.get(url)
 
         folder_path = os.getcwd().replace("\\", "\\\\")
 
-        WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CLASS_NAME, 'ui-pdp-gallery__figure')))
-        element = driver.find_element(By.CLASS_NAME, '/html/body/main/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/span[1]/figure/img')
+        WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '/html/body/main/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/span[1]/figure/img')))
+        element = driver.find_element(By.XPATH, '/html/body/main/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/span[1]/figure/img')
         element.screenshot(f'image-{today.timestamp()}.png')
         image_src = f'{folder_path}/image-{today.timestamp()}.png'
 
-        productTitle = driver.find_element(By.XPATH, '/html/body/main/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/h1').text
-        
-        price_src = ''
-        productPrice = ''
-        productPriceBefore = ''
-        payment= ''
+        element = driver.find_element(By.XPATH, '/html/body/main/div[2]/div[3]/div[2]/div[1]/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/h1')
+        productTitle = element.get_attribute('innerHTML')
 
+        price_src = ''
         try:
             if 'sp' not in update.message.text:
                 element = driver.find_element(By.CLASS_NAME, 'ui-pdp-price__main-container')
                 element.screenshot(f'price-{today.timestamp()}.png')
                 price_src = f'{folder_path}/price-{today.timestamp()}.png'
-
-                productPriceBefore = driver.find_element(By.XPATH, '//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[3]/div[1]/span/s/span[2]').text
-                productPrice = driver.find_element(By.XPATH, '//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[3]/div[1]/div[1]/span[1]/span/span[2]').text
-                payment = driver.find_element(By.XPATH, '//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[3]/div[1]/div[2]').get_attribute('innerText').strip().replace('\n','')
-                
         except Exception as error:
             print("Error finding price", error)
 
         hti = Html2Image()
-
         html = """
         <!DOCTYPE html>
         <html lang="en">
@@ -84,16 +69,14 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                         
                 .internal-div {
                     margin: 0px 0px 0px 0px;
-                    height: 1166px;
+                    height: 1599px;
                     width: 899px;"""+ f"""
-                    background-image: url("{folder_path}/background_small.jpg");"""+"""
+                    background-image: url("{folder_path}/background.jpg");"""+"""
                 }
-
                 .product-div {
                     padding: 150px 5px 5px 5px;
                     min-height: 680px;
                 }
-
                 .price-div {
                     
                     box-sizing: border-box;
@@ -106,19 +89,16 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     text-size-adjust: 100%;
                     width: auto;
                 }
-
                 .old-price-div {
                     text-align: center;
                     font-size: 25px;
                     margin: 0px 0px 0px 0px;
                     color: rgb(86, 89, 89);
                 }
-
                 .price {
                     text-align: center;
                     padding-left: 50px;
                 }
-
                 .old-price {
                     text-decoration: line-through;
                 }
@@ -138,7 +118,6 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     -webkit-line-clamp: 4;
                     -webkit-box-orient: vertical;
                 }
-
                 .product-img {
                     /* Background pattern from Toptal Subtle Patterns */
                     display: block;
@@ -150,7 +129,7 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         image_tag = ''
         if price_src != '':
-            image_tag = f"""<img src="{price_src}" class=product-img width="650px">"""
+            image_tag = f"""<img src="{price_src}" class=product-img width="750px">"""
 
         html += f"""
             <body class="body">
@@ -163,21 +142,17 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     <p class="price">{image_tag}</p>  
                 </div>
                 </div>
-
             </body>
         </html>"""
 
         print(html)
-        text_file = open("output.html", "w")
-        text_file.write(html)
-        text_file.close()
         # screenshot an HTML string (css is optional)
         hti = Html2Image(custom_flags=['--no-sandbox'])
-        hti.screenshot(html_str=html, save_as=f'{today.timestamp()}.png', size=(899, 1166))
-        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=f"{today}.png",caption=f"🛍️✨{productTitle}\n\n<s>R${productPriceBefore}</s>\nR${productPrice}🚨🚨🔥😱🏃🏻‍♀️\n💳 {payment}\n\n<a href='{url}'>🛒 CLIQUE AQUI PARA COMPRAR 👉</a>\n\n<i>*Promoção sujeita a alteração a qualquer momento</i>",parse_mode='HTML',photo=open(f"{folder_path}/{today.timestamp()}.png", "rb"))
+        hti.screenshot(html_str=html, save_as=f'{today.timestamp()}.png', size=(899, 1599))
+        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=f"{today}.png",photo=open(f"{folder_path}/{today.timestamp()}.png", "rb"))
     except Exception as error:
         print("Erro ao gerar imagem", error)
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Erro ao gerar imagem!")
     finally:
-        #deleteTempFiles(today.timestamp())
+        deleteTempFiles(today.timestamp())
         driver.quit()
