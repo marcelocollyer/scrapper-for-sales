c0d89cfa8b7bd0cfa0ea4560443828901aecf013
Marcelo Collyer <marcelocollyer@gmail.com>
2024-04-20 16:27:57 -0400

    lots of improvements to code quality on magalu_bulk operation

--- magalu_bulk.py
+++ magalu_bulk.py
@@ -9,6 +9,149 @@ from telegram.ext import ContextTypes
 import os
 import time
 from datetime import datetime
+import requests
+import json
+import requests
+import boto3
+import jinja2
+
+async def sendDailyPromo(driver, update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    # initializing jinja
+    folder_path = os.getcwd()
+    template_loader = jinja2.FileSystemLoader('.')
+    template_env = jinja2.Environment(loader=template_loader)
+    template = template_env.get_template('magalu_bulk_template.html')
+    
+    # Navigates to promotion website
+    driver.get(os.environ.get('OFERTAS_URL'))
+    
+    # Scrap the product list
+    products = WebDriverWait(driver, 100).until(EC.presence_of_element_located(((By.XPATH, '//*[@id="__next"]/div/main/section[4]/div[2]/div/ul'))))
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located(((By.CLASS_NAME, 'text-button-cookie')))).click()
+    products = products.find_elements(By.TAG_NAME, 'li')
+
+    # Extract details from each product
+    for i, product in enumerate(products):
+        link = product.find_element(By.TAG_NAME, 'a')
+        product_url = link.get_attribute('href')
+        original_image_path = f'original-image-{i+1}.png'
+        product.screenshot(original_image_path)
+
+        # Sends the small background type image
+        image_path = f'image-{i+1}.png'
+        caption = f"<a href='{product_url}'>{product_url}</a>"
+        data = {
+            'image_path': folder_path + '/' + original_image_path,
+            'height': '1599',
+            'background_img_name': folder_path + '/background'
+        }
+        html = template.render(data)
+        print(html)
+        hti = Html2Image(custom_flags=['--no-sandbox'])
+        hti.screenshot(html_str=html, save_as=image_path, size=(899, 1599))
+        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=f"{image_path}",caption=caption,parse_mode='HTML',photo=open(f"{image_path}", "rb"))    
+
+        # gather the product information
+        productTitle = product.find_element(By.CSS_SELECTOR, '[data-testid="product-title"]').text
+        productPriceBefore = get_element_text_or_innerhtml(product, '[data-testid="price-original"]')
+        productPrice = get_element_text_or_innerhtml(product, '[data-testid="price-value"]')
+        payment = get_element_text_or_innerhtml(product, '[data-testid="installment"]')
+
+        # Set the photo and text content
+        photo_url = f"{folder_path}/{image_path}"
+        caption = f"🛍️🛒{productTitle}\n\n<s>{productPriceBefore}</s>\n{productPrice}🚨🚨🔥😱🏃🏻‍♀️\n💳 {payment}\n\n<a href='{product_url}'>🛒 CLIQUE AQUI PARA COMPRAR</a>\n\n<i>*Promoção sujeita a alteração a qualquer momento</i>"
+        
+        # TODO uncomment this once IG business account gets created
+        # Upload file to s3 as with public access and uses the public url to post on instagram stories
+        #access_token = os.environ.get('INSTAGRAM_ACCESS_TOKEN')
+        #instagram_business_account_id = os.environ.get('INSTAGRAM_BUSINESS_ACCOUNT_ID')
+        #upload_file_to_s3(photo_url, 'dicemedice', 'story.jpg')
+        #post_to_facebook(instagram_business_account_id, access_token, text, product_url)
+
+        # Sends the normal background type image
+        data = {
+            'image_path': folder_path + '/' + original_image_path,
+            'height': '1166',
+            'background_img_name': folder_path + '/background_small'
+        }
+        html = template.render(data)
+        print(html)
+        hti.screenshot(html_str=html, save_as=image_path, size=(899, 1166))
+        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=f"magalu.png",caption=caption,parse_mode='HTML',photo=open(f"{image_path}", "rb"))
+        
+        # Clear out the temp files
+        deleteTempFiles(i+1)
+        
+        # this avoids telegram from being flooded
+        time.sleep(3)
+
+def get_element_text_or_innerhtml(element, css_selector):
+    try:
+        return element.find_element(By.CSS_SELECTOR, css_selector).get_attribute('innerHTML').replace("<!-- -->", "").replace('&nbsp;','')
+    except Exception as error:
+        print(f"Error parsing {css_selector}: {error}")
+        return ''
+
+def post_to_facebook(instagram_business_account_id, access_token, text, product_url):
+    
+    # Construct the API request
+    url = f"https://graph.facebook.com/v19.0/{instagram_business_account_id}/media"
+    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
+    
+    data = {
+        "caption": text,
+        "image_url": os.environ.get('S3_TEMP_IMAGE_PATH'),
+        "media_type": "STORIES",
+        "links": [
+            {
+                "url": product_url,
+                "caption": "🛒 CLIQUE AQUI PARA COMPRAR"
+            }
+        ]
+    }
+    
+    # Send the request
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 200:
+        print("Part A of the post successful!")
+        
+        # Get the creation ID from the previous response
+        creation_id = json.loads(response.text)["id"]
+        
+        # Construct the API request for publishing the media
+        url = f"https://graph.facebook.com/v19.0/{instagram_business_account_id}/media_publish"
+        params = {"creation_id": creation_id}
+        
+        # Send the request
+        response = requests.post(url, headers={"Authorization": f"Bearer {access_token}"}, params=params)
+        
+        if response.status_code == 200:
+            print("Media published successfully!")
+        else:
+            print(f"Error: {response.text}")
+    else:
+        print(f"Error: {response.text}")
+
+def upload_file_to_s3(local_path, bucket_name, file_name):
+    # Set up your AWS credentials
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+    # Create an S3 client
+    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    # Upload the file to S3
+    with open(local_path, 'rb') as data:
+        s3.put_object(Body=data, Bucket=bucket_name, Key=file_name)
+
+    # Make the file publicly accessible
+    s3.put_object_acl(Bucket=bucket_name, Key=file_name, ACL='public-read')
+
+    # Print success message
+    print(f"File uploaded successfully: {file_name}")   
 
 def deleteTempFiles(index):
     if os.path.exists(f"original-image-{index}.png"):
@@ -20,145 +163,3 @@ def deleteTempFiles(index):
         os.remove(f"image-{index}.png")
     else:
         print("The file does not exist")
-
-async def sendDailyPromo(driver, update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    folder_path = os.getcwd()
-    driver.get('https://www.magazinevoce.com.br/magazinelojapexincha/selecao/ofertasdodia/?filters=review---4%2Bseller---magazineluiza')
-
-    products = WebDriverWait(driver, 100).until(EC.presence_of_element_located(((By.XPATH, '//*[@id="__next"]/div/main/section[4]/div[2]/div/ul'))))
-    WebDriverWait(driver, 100).until(EC.presence_of_element_located(((By.CLASS_NAME, 'text-button-cookie')))).click()
-
-    # Find all product li elements within the ul list
-    products = products.find_elements(By.TAG_NAME, 'li')
-
-    # Extract details from each product
-    for i, product in enumerate(products):
-        link = product.find_element(By.TAG_NAME, 'a')
-        product_url = link.get_attribute('href')
-        original_image_path = f'original-image-{i+1}.png'
-        product.screenshot(original_image_path)
-
-        image_path = f'image-{i+1}.png'
-        hti = Html2Image(custom_flags=['--no-sandbox'])
-        html = getHTML(original_image_path, folder_path, 'background', '1599')
-        hti.screenshot(html_str=html, save_as=image_path, size=(899, 1599))
-        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=f"{image_path}",caption=f"<a href='{product_url}'>{product_url}</a>",parse_mode='HTML',photo=open(f"{image_path}", "rb"))    
-
-        productTitle = product.find_element(By.CSS_SELECTOR, '[data-testid="product-title"]').text
-        
-        productPriceBefore = ''
-        productPrice = ''
-        payment = ''
-        
-        try:
-            productPriceBefore = product.find_element(By.CSS_SELECTOR, '[data-testid="price-original"]').get_attribute('innerHTML').replace("<!-- -->", "").replace('&nbsp;','')
-        except Exception as error:
-            print("Error parsing previous price ", error)
-        try:                
-            productPrice =  product.find_element(By.CSS_SELECTOR, '[data-testid="price-value"]').get_attribute('innerHTML').replace("<!-- -->", "").replace('&nbsp;','')
-        except Exception as error:
-            print("Error parsing price ", error)    
-        try:    
-            payment = product.find_element(By.CSS_SELECTOR, '[data-testid="installment"]').get_attribute('innerHTML').replace("<!-- -->", "").replace('&nbsp;','')
-        except Exception as error:
-            print("Error parsing payment methods ", error)
-
-        html = getHTML(original_image_path,folder_path, 'background_small', '1166')
-        hti.screenshot(html_str=html, save_as=image_path, size=(899, 1166))
-        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=f"magalu.png",caption=f"🛍️🛒{productTitle}\n\n<s>{productPriceBefore}</s>\n{productPrice}🚨🚨🔥😱🏃🏻‍♀️\n💳 {payment}\n\n<a href='{product_url}'>🛒 CLIQUE AQUI PARA COMPRAR</a>\n\n<i>*Promoção sujeita a alteração a qualquer momento</i>",parse_mode='HTML',photo=open(f"{image_path}", "rb"))
-        
-        deleteTempFiles(i+1)
-        
-        time.sleep(3)
-
-def getHTML(image_path, folder_path, background_img_name, height):
-    html = """
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <title>Product Information</title>
-            </head>
-            <style>          
-                .body {
-                    margin: 0px;
-                }
-                        
-                .internal-div {
-                    margin: 0px 0px 0px 0px;
-                    """+ f"""height: {height}px;
-                    """+"""width: 899px;"""+ f"""
-                    background-image: url('{folder_path}/{background_img_name}.jpg');"""+"""
-                }
-
-                .product-div {
-                    padding: 150px 5px 5px 5px;
-                    min-height: 680px;
-                }
-
-                .price-div {
-                    
-                    box-sizing: border-box;
-                    color: rgb(15, 17, 17);
-                    display: inline;
-                    font-family: Arial, sans-serif;
-                    font-size: 28px;
-                    height: auto;
-                    line-height: normal;
-                    text-size-adjust: 100%;
-                    width: auto;
-                }
-
-                .old-price-div {
-                    text-align: center;
-                    font-size: 25px;
-                    margin: 0px 0px 0px 0px;
-                    color: rgb(86, 89, 89);
-                }
-
-                .price {
-                    text-align: center;
-                    padding-left: 50px;
-                }
-
-                .old-price {
-                    text-decoration: line-through;
-                }
-                
-                .title {
-                    margin-left: auto;
-                    margin-right: auto;
-                    text-align: center;
-                    max-width: 650px;
-                    font-family: serif;
-                    font-size: 45px;
-                    text-align: left;
-                    color: #4a4a4a;
-                    overflow: hidden;
-                    display: -webkit-box;
-                    -webkit-line-clamp: 4;
-                    -webkit-box-orient: vertical;
-                }
-
-                .product-img {
-                    /* Background pattern from Toptal Subtle Patterns */
-                    display: block;
-                    margin-left: auto;
-                    margin-right: auto;
-                    max-width: 767px;
-                    height: 945px;
-                }
-
-            </style>"""
-
-    html += f"""
-        <body class="body">
-            <div class="internal-div">
-            <div class="product-div">
-                <img src="{folder_path}/{image_path}" class="product-img">
-            </div>
-            </div>
-        </body>
-    </html>"""        
-    return html
