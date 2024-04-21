4a7652f93dfec766bcd695d530d56873b69dec4d
Marcelo Collyer <marcelocollyer@gmail.com>
2024-04-21 02:48:09 -0400

    improving natura code

--- scrapper/natura.py
+++ scrapper/natura.py
@@ -3,11 +3,114 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from html2image import Html2Image
 from telegram import Update
 from telegram.ext import ContextTypes
 import os
 from datetime import datetime
+import jinja2
+
+async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Processando...")
+    print("webscrapper is running...")
+    today = datetime.now()
+
+    try:
+        # Config driver        
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--window-size=1920,1400")
+        driver = webdriver.Chrome(options=options)
+
+        # initializing jinja
+        folder_path = os.getcwd()
+        template_loader = jinja2.FileSystemLoader('.')
+        template_env = jinja2.Environment(loader=template_loader)
+        template = template_env.get_template('templates/natura.html.j2')
+
+        # Gets the url from the request
+        url = get_url(update)
+        driver.get(url)
+
+        # scraps product image, url and title
+        element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[1]/div[1]/div[2]')))
+        element.screenshot(f'image-{today.timestamp()}.png')
+        image_src = f'{folder_path}/image-{today.timestamp()}.png'
+        productTitle = driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/h1').get_attribute('innerHTML')
+        
+        # if sp has been provided as parameter, no need to find the prices, otherwise...
+        if ' sp' not in update.message.text[-3:]:
+            element = driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]')
+            element.screenshot(f'price-{today.timestamp()}.png')
+            price_src = f'{folder_path}/price-{today.timestamp()}.png'
+
+            xpath_product_price_before = '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]/div[1]/p'
+            xpath_product_price = '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]/div[1]/div'
+            xpath_payment = '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]/div[2]'
+
+            productPriceBefore = get_element_text(driver, xpath_product_price_before)
+            productPrice = get_element_text(driver, xpath_product_price)
+            payment = get_element_text(driver, xpath_payment)
+
+        # generates and sends to telegram the normal size image
+        path = f'{today.timestamp()}.png'
+        hti = Html2Image(custom_flags=['--no-sandbox'])
+        html = template.render({
+            'price_src': price_src,
+            'image_src': image_src, 
+            'productTitle': productTitle,
+            'background_img_name': folder_path + '/image/background', 
+            'height': '1599'
+        })
+        print(html)
+        hti.screenshot(html_str=html, save_as=path, size=(899, 1599))
+        caption = f"<a href='{url}'>{url}</a>"
+        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=path,caption=caption,parse_mode='HTML',photo=open(path, "rb"))
+
+        hti = Html2Image(custom_flags=['--no-sandbox'])
+        html = template.render({
+            'price_src': price_src,
+            'image_src': image_src, 
+            'productTitle': productTitle,
+            'background_img_name': folder_path + '/image/background_small', 
+            'height': '1166'
+        })
+        print(html)
+        hti.screenshot(html_str=html, save_as=path, size=(899, 1166))
+        caption = f"🛍️🛒{productTitle}\n\n<s>{productPriceBefore}</s>\n{productPrice}🚨🚨🔥😱🏃🏻‍♀️\n💳 {payment}\n\n<a href='{url}'>🛒 CLIQUE AQUI PARA COMPRAR</a>\n\n<i>*Promoção sujeita a alteração a qualquer momento</i>"
+        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=path,caption=caption,parse_mode='HTML',photo=open(f"{folder_path}/{today.timestamp()}.png", "rb"))
+
+    except Exception as error:
+        print("Erro ao gerar imagem", error)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Erro ao gerar imagem!")
+    finally:
+        deleteTempFiles(today.timestamp())
+        driver.quit()
+
+def get_url(update):
+    try:
+        url_parts = update.message.text.split()
+        if len(url_parts) > 1:
+            return url_parts[1]
+        else:
+            return ''
+    except Exception as error:
+        print("Error parsing URL: ", error)
+        return ''       
+
+def get_element_text(driver, xpath, label="element"):
+    
+    try:
+        return driver.find_element(By.XPATH, xpath).text.replace('\n', '')
+    except NoSuchElementException:
+        print(f"Error: {label} not found.")
+        return ''
+    except WebDriverException as e:
+        print(f"Error parsing {xpath}: {e}")
+        return ''
 
 def deleteTempFiles(date_time):
     if os.path.exists(f"{date_time}.png"):
@@ -24,169 +127,3 @@ def deleteTempFiles(date_time):
         os.remove(f"image-{date_time}.png")
     else:
         print("The file does not exist")
-
-async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Processando...")
-    print("webscrapper is running...")
-    today = datetime.now()
-
-    try:
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--window-size=1920,1400")
-        driver = webdriver.Chrome(options=options)
-
-        url = update.message.text.split()[1]
-        driver.get(url)
-
-        folder_path = os.getcwd().replace("\\", "\\\\")
-        
-        element = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[1]/div[1]/div[2]')))
-        element.screenshot(f'image-{today.timestamp()}.png')
-        image_src = f'{folder_path}/image-{today.timestamp()}.png'
-
-        element = driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/h1')
-        productTitle = element.get_attribute('innerHTML')
-
-        price_src = ''
-        productPriceBefore = ''
-        productPrice = ''
-        payment = ''
-
-        if ' sp' not in update.message.text[-3:]:
-            element = driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]')
-            element.screenshot(f'price-{today.timestamp()}.png')
-            price_src = f'{folder_path}/price-{today.timestamp()}.png'
-
-            try:
-                productPriceBefore = driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]/div[1]/p').text.replace('\n','')
-            except Exception as error:
-                print("Error parsing previous price ", error)
-            try:                
-                productPrice =  driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]/div[1]/div').text.replace('\n','')
-            except Exception as error:
-                print("Error parsing price ", error)    
-            try:    
-                payment = driver.find_element(By.XPATH, '//*[@id="sticky-container"]/div[2]/div/div/div[2]/div/div[3]/div[2]').text.replace('\n','')
-            except Exception as error:
-                print("Error parsing payment methods ", error)    
-            
-
-        path = f'{today.timestamp()}.png'
-        hti = Html2Image(custom_flags=['--no-sandbox'])
-        html = getHTML(price_src, image_src,productTitle,folder_path, 'background', '1599')
-        hti.screenshot(html_str=html, save_as=path, size=(899, 1599))
-        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=path,caption=f"<a href='{url}'>{url}</a>",parse_mode='HTML',photo=open(path, "rb"))
-
-        hti = Html2Image(custom_flags=['--no-sandbox'])
-        html = getHTML(price_src, image_src,productTitle,folder_path, 'background_small', '1166')
-        hti.screenshot(html_str=html, save_as=path, size=(899, 1166))
-        await context.bot.send_photo(chat_id=update.effective_chat.id,filename=path,caption=f"🛍️🛒{productTitle}\n\n<s>{productPriceBefore}</s>\n{productPrice}🚨🚨🔥😱🏃🏻‍♀️\n💳 {payment}\n\n<a href='{url}'>🛒 CLIQUE AQUI PARA COMPRAR</a>\n\n<i>*Promoção sujeita a alteração a qualquer momento</i>",parse_mode='HTML',photo=open(f"{folder_path}/{today.timestamp()}.png", "rb"))
-
-    except Exception as error:
-        print("Erro ao gerar imagem", error)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Erro ao gerar imagem!")
-    finally:
-        deleteTempFiles(today.timestamp())
-        driver.quit()
-
-def getHTML(price_src, image_src, productTitle, folder_path, background_img_name, height):
-
-    html = """
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <title>Product Information</title>
-            </head>
-            <style>          
-                .body {
-                    margin: 0px;
-                }
-                        
-                .internal-div {
-                    margin: 0px 0px 0px 0px;
-                    """+ f"""height: {height}px;
-                    """+"""width: 899px;"""+ f"""
-                    background-image: url('{folder_path}/image/{background_img_name}.jpg');"""+"""
-                }
-
-                .product-div {
-                    padding: 150px 5px 5px 5px;
-                    min-height: 680px;
-                }
-
-                .price-div {
-                    
-                    box-sizing: border-box;
-                    color: rgb(15, 17, 17);
-                    display: inline;
-                    font-family: Arial, sans-serif;
-                    font-size: 28px;
-                    height: auto;
-                    line-height: normal;
-                    text-size-adjust: 100%;
-                    width: auto;
-                }
-
-                .old-price-div {
-                    text-align: center;
-                    font-size: 25px;
-                    margin: 0px 0px 0px 0px;
-                    color: rgb(86, 89, 89);
-                }
-
-                .price {
-                    text-align: center;
-                    padding-left: 50px;
-                }
-
-                .old-price {
-                    text-decoration: line-through;
-                }
-                
-                .title {
-                    margin-left: auto;
-                    margin-right: auto;
-                    text-align: center;
-                    max-width: 650px;
-                    font-family: serif;
-                    font-size: 45px;
-                    text-align: left;
-                    color: #4a4a4a;
-                    overflow: hidden;
-                    display: -webkit-box;
-                    -webkit-line-clamp: 4;
-                    -webkit-box-orient: vertical;
-                }
-
-                .product-img {
-                    /* Background pattern from Toptal Subtle Patterns */
-                    display: block;
-                    margin-left: auto;
-                    margin-right: auto;
-                    max-width: 820px;
-                }
-            </style>"""
-
-    img_tag = ''
-    if price_src != '':
-        img_tag = f"<img src='{price_src}' class=product-img width='750px'>"
-
-    html += f"""
-        <body class="body">
-            <div class="internal-div">
-            <div class="product-div">
-                <img src="{image_src}" alt="Product Image" class=product-img height="500">
-                <h1 class="title">{productTitle}</h1>
-            </div>
-            <div class="price-div">
-                <p class="price">{img_tag}</p>  
-            </div>
-            </div>
-
-        </body>
-    </html>"""
-    return html        
