55ea4b102fe2db09122451dd8157b24c4e40aeea
Marcelo Collyer <marcelocollyer@gmail.com>
2024-05-04 15:56:47 -0400

    replacing pt-br words

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,10 +2,10 @@
 
 while true; do
   if ! curl -f http://localhost:8080/health; then
-    echo "O serviço não está respondendo, reiniciando o container..."
+    echo "Service not responding. Container will get recycled..."
     docker restart sales
   else
-    echo "O serviço está operacional."
+    echo "Service is up!"
   fi
   sleep 30
 done
